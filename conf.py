b6a8c4857c4bb6a107416ab5d36c2afbacce88da
amazonview <chetijasimran@gmail.com>
2025-06-30 12:30:38 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'Max Providers'
 
 
 # The full version, including alpha/beta/rc tags
